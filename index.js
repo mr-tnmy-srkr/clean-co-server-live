9a2cbd2cd55c31aaccbe86c704ea585c6559e236
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-11-12 12:06:39 +0530

    deploy successful

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.use(
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@myprojectscluster.drcktji.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@myprojectscluster.drcktji.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://tnmy_srkr:877smORJET4ct5n4@myprojectscluster.drcktji.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
